b3eeaf19da52a32a1ff8506673d78a52d7496f52
Yauhen Pahrabniak <emcifuntik@gmail.com>
2023-11-01 15:46:10 +0100

    Removed logs

--- src/router.ts
+++ src/router.ts
@@ -1,30 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppHome from "./views/AppHome.vue";
 
-const routes = [
-  {
-    path: "/:hash?",
-    name: "home",
-    component: AppHome,
-    props: true,
-    meta: {
-      title: "alt:V / NativeDB",
-    },
-  },
-];
-
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+    {
+      path: "/:hash?",
+      name: "home",
+      component: AppHome,
+      props: true,
+      meta: {
+        title: "alt:V / NativeDB",
+      },
+    },
+  ],
 });
 
 router.beforeEach((to, from, next) => {
   console.log(to, from, next);
   const hashRedirect = /#\/(.*)$/.exec(to.fullPath);
   if (hashRedirect) {
-    console.log(hashRedirect);
     const path = hashRedirect[1];
-    console.log(path);
     return next({ path: `/${path}` });
   } else {
     return next();
